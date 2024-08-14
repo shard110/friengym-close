1572c7b313f5a303f27193944a7d795a784bcc19
myname <roorroo360@gmail.com>
2024-08-14 17:57:55 +0900

    테이블 매핑

--- src/main/java/com/example/demo/entity/Post.java
+++ src/main/java/com/example/demo/entity/Post.java
@@ -1,52 +1,101 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 
 @Entity
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // GenerationType.IDENTITY는 데이터베이스에 따라 자동으로 ID를 생성.
-    @Column(name = "ponum") // 데이터베이스 열 이름
-	private Long poNum;
-    
-    //임시 (jpa가 기존테이블을 수정할 수있어서 상의 후 바꾸겠습니다) ///////////////
-    private String title;
-    private String content;
-    private String username;
-	
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ponum")
+    private Long poNum;
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username; 
+
+    @Column(name = "poTitle", nullable = false, length = 100)
+    private String title; 
+
+    @Column(name = "poContents", nullable = false, length = 4000)
+    private String content; 
+
+    @Column(name = "poDate", updatable = false)
+    private LocalDateTime poDate; 
+
+    @Column(name = "updatedDate")
+    private LocalDateTime updatedDate; // 수정 날짜추가
+
+    public Post() {}
+
+    public Post(String title, String content, String username) {
+        this.title = title;
+        this.content = content;
+        this.username = username;
+    }
+
     public Long getPoNum() {
         return poNum;
     }
-    
+
     public void setPoNum(Long poNum) {
-        this.poNum = poNum; 
+        this.poNum = poNum;
     }
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
+    public String getTitle() {
+        return title;
+    }
 
-    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getPoDate() {
+        return poDate;
+    }
+
+    public void setPoDate(LocalDateTime poDate) {
+        this.poDate = poDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @PrePersist
+    public void onCreate() {
+        this.poDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedDate = LocalDateTime.now();
+    }
 }
-
-    
