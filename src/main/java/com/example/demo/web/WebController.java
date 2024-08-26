6717a773bcce0d5a3f3d653bc69acaf6aad052e6
sowons <thdnjs4763@naver.com>
2024-08-26 10:50:25 +0900

    구조 변경

---
+++ src/main/java/com/example/demo/web/WebController.java
@@ -0,0 +1,14 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/custom-error")
+    public String handleCustomError() {
+        // 커스텀 에러 페이지로 포워딩
+        return "customError";
+    }
+}
