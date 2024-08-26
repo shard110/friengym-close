0206d9ce78885417866a40bbe198b38f72fab583
sowons <thdnjs4763@naver.com>
2024-08-26 10:09:51 +0900

    자바파일 수정

---
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+
+import com.example.demo.dto.Page;
+import com.example.demo.entity.Post;
+
+public interface PostRepository  extends JpaRepository<Post, Integer> {
+    @NonNull
+    Page<Post> findAll(@NonNull Pageable pageable);
+}
