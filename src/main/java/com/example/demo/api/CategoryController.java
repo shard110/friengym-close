5b819bd0640ec6c0935a83cdd460efb27eda0e99
sowons <thdnjs4763@naver.com>
2024-08-26 10:44:12 +0900

    컨트롤러 오류 수정

--- src/main/java/com/example/demo/api/CategoryController.java
+++ src/main/java/com/example/demo/api/CategoryController.java
@@ -1,7 +1,6 @@
 package com.example.demo.api;
 
 import java.util.List;
-import java.util.Locale.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Category;
 import com.example.demo.service.CategoryService;
 
 @RestController
