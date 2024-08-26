43f9dc70a5eaac83d8b1f7936abb05613f26b8ab
sowons <thdnjs4763@naver.com>
2024-08-26 15:21:54 +0900

    그리들 설정

--- src/main/java/com/example/demo/api/PostController.java
+++ src/main/java/com/example/demo/api/PostController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.demo.entity.Post;
 import com.example.demo.service.PostService;
@@ -39,4 +43,16 @@ public class PostController {
 
         return response;
     }
+
+    @RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        // 예외 로그 출력
+        ex.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error: " + ex.getMessage());
+    }
+}
+
 }
