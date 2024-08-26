5751948fdc4fffb3ef8e420199b91c7f3d65b64a
sowons <thdnjs4763@naver.com>
2024-08-26 10:46:30 +0900

    레포지토리 오류 수정

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repository;
 
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 
-import com.example.demo.dto.Page;
 import com.example.demo.entity.Post;
 
 public interface PostRepository  extends JpaRepository<Post, Integer> {
