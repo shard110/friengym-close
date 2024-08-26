0b02edb4831c151d473741c9f0b40fc6f9b18f34
sowons <thdnjs4763@naver.com>
2024-08-26 14:53:50 +0900

    리액트연동

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 
